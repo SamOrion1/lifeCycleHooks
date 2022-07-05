6c6667931e00fc6930e0f3f4e1f605d6dcca2c43
Sam <99719826+SamOrion1@users.noreply.github.com>
2022-07-05 14:46:29 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+        <p id="Demo">{{name}}</p>
+    </div>
+</template>
+
+<script lang="ts">
+
+export default {
+
+    name : '',
+
+    data (){
+        return {name: "Rasengan", display:true}
+    },
+
+    beforeCreate(){
+        this.name="Chidori"
+        console.log("before create", this.name)
+    },
+    
+    created(){
+        // this.name="Chidori"
+        console.log("created", this.name)
+    },
+
+    beforeMount(){
+        console.log("before mount", document.getElementById('Demo'))
+    },
+
+    mounted(){
+        console.log("mounted", document.getElementById('Demo'))
+    },
+
+    beforeUnmount() {
+        console.log('before unmount', this.name)
+    },
+
+    unmount() {
+        console.log('unmount', this.name)
+    },
+    
+}
+</script>
---
+++ src/components/testLch.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+
+
+export default {
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount')
+  },
+  unmounted() {
+    console.log('unmounted')
+  }
+}
+
+
+</script>
